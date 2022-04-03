acf372c22ecf0ea2c2348ce87ebc401bc5f38dfd
zabdiel1 <jonathanalbayero203@hotmail.com>
2022-04-03 11:03:58 -0600

    modificaciones para cliente-servidor

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mysql = require("mysql");
 const myconn = require("express-myconnection");
+const cors = require("cors");
 
 const routes = require("./routes");
 
@@ -14,6 +15,13 @@ const dbOptions = {
  password: "08da3add",
  database: "heroku_92c583a6a747576",
 };
+
+const corsOptions = {
+ origin: "http://localhost:3000",
+ credentials: true, //access-control-allow-credentials:true
+ optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 //mysql://b9236bf3599fa7:08da3add@us-cdbr-east-05.cleardb.net/heroku_92c583a6a747576?reconnect=true
 
